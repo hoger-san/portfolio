ff84ea1c203f33e45e89ebc620dd6ef17d8709b3
hznk <nhanzawa1211@gmail.com>
2020-04-05 17:29:24 +0900

    feat：ナビゲーションのリストを自動生成（一覧ページのリスト）

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,9 +15,9 @@
           router
           exact
         >
-          <v-list-item-action>
+          <!-- <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
+          </v-list-item-action> -->
           <v-list-item-content>
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
@@ -47,23 +47,24 @@ export default {
       clipped: false,
       drawer: false,
       fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'サイトコンテンツ１',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'サイトコンテンツ２',
-          to: '/inspire'
-        }
-      ],
+      items: [],
       miniVariant: false,
       right: true,
       rightDrawer: false,
       title: 'portfolio'
     }
+  },
+  created() {
+    this.$router.options.routes.forEach((route) => {
+      /** 一覧ページのパス */
+      if (!/\/.*?\/.*/.test(route.path)) {
+        this.items.push({
+          title: route.name,
+          to: route.path,
+          icon: ''
+        })
+      }
+    })
   }
 }
 </script>
