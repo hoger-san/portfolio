a695319c3bc72e276e7a0a525d4de26c41713c1d
hznk <nhanzawa1211@gmail.com>
2020-04-05 20:54:23 +0900

    feat：SPAモードでエラー時は404.htmlを表示する設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -84,6 +84,9 @@ export default {
      */
     extend(config, ctx) {}
   },
+  generate: {
+    fallback: true
+  },
   env: {
     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
     CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
