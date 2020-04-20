efea70f790f6d65e76a02be24f00d93f34779d51
hznk <nhanzawa1211@gmail.com>
2020-04-20 22:27:21 +0900

    feat：記事マークアップの修正

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -4,12 +4,22 @@
     <v-col xs="12" sm="10" md="10" lg="8">
       <article>
         <v-img :src="post.fields.heroImage.fields.file.url"></v-img>
-        <time class="caption">
-          {{ new Date(post.fields.publishDate).toDateString() }}
-        </time>
-        <h1 class="display-2">{{ post.fields.title }}</h1>
-        <p class="headline text--secondary">{{ post.fields.description }}</p>
-        <div v-html="$md.render(post.fields.body)"></div>
+        <section class="headlines">
+          <time class="caption">
+            {{ new Date(post.fields.publishDate).toDateString() }}
+          </time>
+          <h1 class="display-1">{{ post.fields.title }}</h1>
+          <p class="headline text--secondary">{{ post.fields.description }}</p>
+        </section>
+        <section class="body" v-html="$md.render(post.fields.body)"></section>
+        <v-chip-group>
+          <nuxt-link
+            v-for="tag in post.fields.tags"
+            :key="tag"
+            :to="{ name: 'tags-tag', params: { tag: tag } }"
+            ><v-chip>{{ tag }}</v-chip></nuxt-link
+          >
+        </v-chip-group>
       </article>
     </v-col>
     <v-col xs="0" sm="1" md="1" lg="2"></v-col>
@@ -35,3 +45,8 @@ export default {
   }
 }
 </script>
+<style scoped>
+section {
+  margin-bottom: 3em;
+}
+</style>
